ad84f7bfdd50e602791ca01b8555cbec95638fda
ronald <r.gaskin@live.ca>
2019-05-21 14:43:36 -0700

    add theme files

---
+++ themes/inhabitent/footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+</div><!-- #content -->
+<p>CONTACT INFO
+	<br>
+	info@inhabitent.com
+	<br>
+	778-456-7891</p>
+<p>BUSINESS HOURS
+	<br>
+	Monday-Friday: 9am to 5pm
+	<br>
+	Saturday: 10am to 2pm
+	<br>
+	Sunday: Closed</p>
+	
+<center>COPYRIGHT © 2019 INHABITENT</center>
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="site-info">
+		
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
