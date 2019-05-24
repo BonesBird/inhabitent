66eca6de8ca24dca886f1c01c89b229f778066c4
ronald <r.gaskin@live.ca>
2019-05-24 10:37:31 -0700

    footer-image

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -8,24 +8,38 @@
 ?>
 
 </div><!-- #content -->
-<p>CONTACT INFO
-	<br>
-	info@inhabitent.com
-	<br>
-	778-456-7891</p>
-
-<p>BUSINESS HOURS
-	<br>
-	Monday-Friday: 9am to 5pm
-	<br>
-	Saturday: 10am to 2pm
-	<br>
-	Sunday: Closed</p>
-
-<center>COPYRIGHT © 2019 INHABITENT</center>
-
-<footer id="colophon" class="site-footer" role="contentinfo">
+<footer id="colophon" class="site-footer" role="contentinfo" style="background-image: url(wp-content/themes/inhabitent/project-04-2/images/dark-wood.png)">
 	<div class="site-info">
+	
+		<div class="footer-block container">
+			<div class="footer-item-1">
+				<div class="contact-info">
+					<p>
+						CONTACT INFO
+						<br>
+						info@inhabitent.com
+						<br>
+						778-456-7891
+					</p>
+				</div>
+			</div>
+			<div class="footer-item-2">
+				<div class="hours-info">
+					<p>
+						BUSINESS HOURS
+						<br>
+						Monday-Friday: 9am to 5pm
+						<br>
+						Saturday: 10am to 2pm
+						<br>
+						Sunday: Closed
+					</p>
+				</div>
+			</div>
+
+			<center>COPYRIGHT © 2019 INHABITENT</center>
+		</div>
+	</div>
 
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
