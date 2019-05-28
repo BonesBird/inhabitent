03d2e4aa33a041aec6841eb850766baa62d28598
ronald <r.gaskin@live.ca>
2019-05-28 09:30:12 -0700

    working-on-products

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -1,57 +1,43 @@
 <?php
 /**
- * Template part for displaying posts.
+ * The template for displaying archive pages.
  *
  * @package RED_Starter_Theme
  */
 
-?>
+get_header(); ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <?php if (has_post_thumbnail()) : ?>
-            <?php the_post_thumbnail('large'); ?>
-        <?php endif; ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-        <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
+        <?php if (have_posts()) : ?>
 
-        <?php if ('post' === get_post_type()) : ?>
-            <div class="entry-meta">
-                <?php red_starter_posted_on(); ?> / <?php comments_number('0 Comments', '1 Comment', '% Comments'); ?> / <?php red_starter_posted_by(); ?>
-            </div><!-- .entry-meta -->
-        <?php endif; ?>
-    </header><!-- .entry-header -->
-
-    <div class="entry-content">
-        <?php the_excerpt(); ?>
-    </div><!-- .entry-content -->
-</article><!-- #post-## -->
-
-
-
-
-<section>
-    <h2>Shop Stuff<h2>
-            <?php
-            $terms = get_terms(
-                array('taxonomy' => 'product-type', 'hide_empty' => 0)
-            ); ?>
-            <!-- begining of product terms -->
-            <div class='product-terms'>
+            <header class="page-header">
                 <?php
-                foreach ($terms as $term) : ?>
-                    <li class="product-term">
-                        <p><?php echo $term->description; ?></p>
-                        <p>
-                            <a href="<?php echo get_term_link($term); ?>">
-                                <?php echo $term->name; ?> Stuff
-                            </a>
-                        </p>
-                    <?php
-                endforeach;
+                the_archive_title('<h1 class="page-title">', '</h1>');
+                the_archive_description('<div class="taxonomy-description">', '</div>');
                 ?>
-            </div>
-            <!-- end of product terms -->
-</section>
+            </header><!-- .page-header -->
+
+            <?php /* Start the Loop */ ?>
+            <?php while (have_posts()) : the_post(); ?>
+
+                <?php
+                get_template_part('template-parts/content');
+                ?>
+
+            <?php endwhile; ?>
+
+            <?php the_posts_navigation(); ?>
+
+        <?php else : ?>
+
+            <?php get_template_part('template-parts/content', 'none'); ?>
+
+        <?php endif; ?>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
 
 <?php get_footer(); ?>
