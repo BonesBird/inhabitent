187798d8c7f7e82d71c47d6c2444bcd7244e4508
ronald <r.gaskin@live.ca>
2019-06-03 15:25:15 -0700

    finished-main-banner-kida

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -10,42 +10,41 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 		<section class="home-hero">
-			<!-- <a href='project-04-2/images/logos/inhabitent-logo-full.svg'></a> -->
-			<img src="project-04-2/images/logos/inhabitent-logo-full.svg" alt="">
+			<img src="<?php echo get_template_directory_uri() . ('/project-04-2/images/logos/inhabitent-logo-full.svg') ?>">
 		</section>
 		<section browse-shop>
-			<h2>Shop Stuff<h2>
+			<h2 style="padding-left: 653px;">Shop Stuff</h2>
+			<?php
+			$terms = get_terms(
+				array('taxonomy' => 'product-type', 'hide_empty' => 0)
+			); ?>
+			<!-- begining of product terms -->
+			<div class='product-terms'>
+				<ul>
 					<?php
-					$terms = get_terms(
-						array('taxonomy' => 'product-type', 'hide_empty' => 0)
-					); ?>
-					<!-- begining of product terms -->
-					<div class='product-terms'>
-						<ul>
-							<?php
-							foreach ($terms as $term) : ?>
-								<li class="product-term" style="list-style-type: none;">
-									<img src="<?php echo get_template_directory_uri() .
-													'/project-04-2/images/product-type-icons/' .
-													$term->slug . '.svg'
-												?>" />
-									<!-- links of products -->
-									<p><?php echo $term->description; ?></p>
-									<p>
-										<a href="<?php echo get_term_link($term); ?>">
-											<?php echo $term->name; ?>
-										</a>
-									</p>
-								</li>
-							<?php
-						endforeach;
-						?>
-						</ul>
-					</div>
-
+					foreach ($terms as $term) : ?>
+						<li class="product-term" style="list-style-type: none;">
+							<img src="<?php echo get_template_directory_uri() .
+											'/project-04-2/images/product-type-icons/' .
+											$term->slug . '.svg'
+										?>" />
+							<!-- links of products -->
+							<p><?php echo $term->description; ?></p>
+							<p>
+								<a href="<?php echo get_term_link($term); ?>">
+									<?php echo $term->name; ?>
+								</a>
+							</p>
+						</li>
+					<?php
+				endforeach;
+				?>
+				</ul>
+			</div>
+		</section>
 </div>
-<!-- end of product terms -->
-</section>
+
+
 
 
 </main><!-- #main -->
