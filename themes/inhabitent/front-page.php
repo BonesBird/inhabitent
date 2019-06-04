7a8186e9681f8abf7e3e202cabd3ffb31e41fad0
ronald <r.gaskin@live.ca>
2019-06-04 11:26:38 -0700

    front-page-work-And-find-us-page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -9,11 +9,13 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
+
 		<section class="home-hero">
 			<img src="<?php echo get_template_directory_uri() . ('/project-04-2/images/logos/inhabitent-logo-full.svg') ?>">
 		</section>
+
 		<section browse-shop>
-			<h2 style="padding-left: 653px;">Shop Stuff</h2>
+			<h2>shop stuff</h2>
 			<?php
 			$terms = get_terms(
 				array('taxonomy' => 'product-type', 'hide_empty' => 0)
@@ -42,6 +44,50 @@ get_header(); ?>
 				</ul>
 			</div>
 		</section>
+
+		<section class="new-entrys">
+			<h2>Inhabitent Journal</h2>
+			<article class="grid-container">
+				<?php
+				$args = array(
+					'post_type' => 'post',
+					'numberposts' => '3',
+				);
+				$journal_posts = get_posts($args);
+				?>
+				<?php foreach ($journal_posts as $post) : setup_postdata($post); ?>
+
+					<div class="grid-item newContent">
+						<?php if (has_post_thumbnail()) : ?>
+							<a class="journal-thumbnail" href="<?php the_permalink(); ?>" title="<?php the_title() ?>">
+								<?php the_post_thumbnail('large'); ?>
+							</a>
+
+						<?php endif; ?>
+
+						<!-- post author and date -->
+						<div class="grid-item entryTitle-btn">
+							<div class="fp-post-meta">
+								<?php echo get_the_date(); ?>
+								<?php echo get_comments_number(); ?> Comments
+							</div>
+
+							<!-- post title -->
+
+							<h3>
+								<a href="<?php the_permalink(); ?>"><?php the_title() ?></a>
+							</h3>
+							<div class="grid-item readMore-btn">
+								<a href="<?php the_permalink(); ?>">Read Entry</a>
+							</div>
+						</div>
+					</div>
+				<?php endforeach;
+			wp_reset_postdata(); ?>
+			</article>
+
+		</section>
+
 </div>
 
 
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -24,6 +24,7 @@
 		<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html('Skip to content'); ?></a>
 
 		<header id="masthead" class="site-header" role="banner">
+
 			<div class="top-logo"></div>
 
 			<div class="site-branding">
