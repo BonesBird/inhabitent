42304bc2e08f1e3edbe7149ac9d3a3eb947b2a60
ronald <r.gaskin@live.ca>
2019-06-05 16:14:08 -0700

    done-fighting-with-java

---
+++ themes/inhabitent/js/script.js
@@ -0,0 +1,31 @@
+// import { PassThrough } from "stream";
+
+// (funtion($) {
+// $(function(){
+//     const icon = $('.search-toggle')
+
+//     $(icon).click(function(event){
+//         event.preventDefault();
+//             const searchInput = $('.search-field')
+
+//         $(searchInput).show(slow)
+//         $("input").focus(function())
+
+//     })
+
+//     //if field is blank
+//     //blur
+//     //.hide
+
+// });
+// })(jQuery);
+
+// better using oldschool method
+// function myFunction() {
+//     var x = document.getElementById("magicTrick");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
