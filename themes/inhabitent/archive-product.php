59e88dba8f5d8bea09fdd716d5f7ed606a5ad884
ronald <r.gaskin@live.ca>
2019-06-05 11:13:36 -0700

    archive-product

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -46,11 +46,19 @@ get_header(); ?>
             <!-- .page-header -->
             <div class="shop-containment">
                 <?php /* Start the Loop */ ?>
+
                 <?php while (have_posts()) : the_post(); ?>
 
-                    <?php
-                    get_template_part('template-parts/content');
-                    ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <header class="entry-header">
+                            <?php if (has_post_thumbnail()) : ?>
+                                <?php the_post_thumbnail('large'); ?>
+
+                            <?php endif; ?>
+
+                            <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
+                        </header><!-- .entry-header -->
+                    </article><!-- #post-## -->
 
                 <?php endwhile; ?>
 
@@ -62,6 +70,10 @@ get_header(); ?>
 
             <?php endif; ?>
         </div>
+
+
+
+
     </main><!-- #main -->
 </div><!-- #primary -->
 
--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -11,29 +11,52 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function red_starter_body_classes( $classes ) {
-	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
+function red_starter_body_classes($classes)
+{
+    // Adds a class of group-blog to blogs with more than 1 published author.
+    if (is_multi_author()) {
+        $classes[] = 'group-blog';
+    }
 
-	return $classes;
+    return $classes;
 }
 
-add_filter( 'body_class', 'red_starter_body_classes' );
+add_filter('body_class', 'red_starter_body_classes');
 
-function inhabitent_remove_submenus() {
-    remove_submenu_page( 'themes.php', 'theme-editor.php' );
-    remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
+function inhabitent_remove_submenus()
+{
+    remove_submenu_page('themes.php', 'theme-editor.php');
+    remove_submenu_page('plugins.php', 'plugin-editor.php');
 }
-add_action( 'admin_menu', 'inhabitent_remove_submenus', 110 );
+add_action('admin_menu', 'inhabitent_remove_submenus', 110);
 
 
-function inhabitent_body_class_for_pages( $classes ) {
-    if ( is_singular( 'page' ) ) {
+function inhabitent_body_class_for_pages($classes)
+{
+    if (is_singular('page')) {
         global $post;
         $classes[] = 'page-' . $post->post_name;
     }
     return $classes;
 }
-add_filter( 'body_class', 'inhabitent_body_class_for_pages' );
+add_filter('body_class', 'inhabitent_body_class_for_pages');
+
+function foo_modify_query_order($query)
+{
+
+    if (is_admin() || !$query->is_main_query())
+        return;
+
+    if (is_home()) {
+        $query->set(‘posts_per_page’, 5);
+        return;
+    }
+
+    if (is_post_type_archive('product')) {
+        $query->set('posts_per_page', 16);
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        return;
+    }
+}
+add_action('pre_get_posts', 'foo_modify_query_order');
