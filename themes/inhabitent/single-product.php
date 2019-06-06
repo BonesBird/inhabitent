4ca102cd431a0dbc4ddea7006dcc6440729710d8
ronald <r.gaskin@live.ca>
2019-06-06 16:28:54 -0700

    fixed-all-the-a-tags

--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -40,7 +40,7 @@ get_header(); ?>
 
                         <div class="entry-content">
                             <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
-                            <?php echo CFS()->get('product_price');  ?>
+                            <span class="price"><?php echo CFS()->get('product_price');  ?></span>
                             <?php the_excerpt(); ?>
                         </div><!-- .entry-content -->
                     </article><!-- #post-## -->
