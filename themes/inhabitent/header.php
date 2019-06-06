b254190690c53d11e34784f9383ad25239857deb
ronald <r.gaskin@live.ca>
2019-06-05 18:40:20 -0700

    working-on-home

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -24,8 +24,11 @@
 		<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html('Skip to content'); ?></a>
 
 		<header id="masthead" class="site-header" role="banner">
+			<a href="<?php echo esc_url(home_url('/')); ?>">
+				<div class="top-logo">
+				</div>
+			</a>
 
-			<div class="top-logo"></div>
 
 			<div class="site-branding">
 				<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -3,7 +3,7 @@
 
 		<fieldset><i class="fas fa-search" onclick="myFunction()"></i>
 			<label>
-				<input type="search" class="search-field" placeholder="SEARCH ..." id="magicTrick" value="<?php echo esc_attr(get_search_query()); ?>" name="s" title="Search for:" />
+				<input type="search" class="search-field" placeholder="SEARCH ..." id="icon" value="<?php echo esc_attr(get_search_query()); ?>" name="s" title="Search for:" />
 			</label>
 			<span class="screen-reader-text"><?php echo esc_html('Search'); ?></span>
 		</fieldset>
@@ -12,7 +12,7 @@
 
 <script>
 	function myFunction() {
-		var x = document.getElementById("magicTrick");
+		var x = document.getElementById("icon");
 		if (x.style.display === "none") {
 			x.style.display = "block";
 		} else {
